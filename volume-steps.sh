9c09e73ddee4c82da96b3e51f26f3304927d520b
Alex <alextec70@outlook.com>
2024-11-11 22:18:33 +0000

    Create volume-steps.sh

---
+++ volume-steps.sh
@@ -0,0 +1,5 @@
+resetprop -n audio.safemedia.bypass true
+resetprop -n ro.config.vc_call_vol_steps 25
+resetprop -n ro.config.media_vol_steps 50
+resetprop -n ro.config.alarm_vol_steps 50
+resetprop -n ro.config.system_vol_steps 50
